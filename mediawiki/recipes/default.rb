906619e1a48b475dd1aa83322ea4c4c159269f78
Requena <>
2013-02-10 13:34:08 +0100

    the mediawiki cookbook

---
+++ mediawiki/recipes/default.rb
@@ -0,0 +1,97 @@
+#
+# Cookbook Name:: mediawiki
+# Recipe:: default
+#
+# Copyright 2011, ccat
+#
+
+::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
+include_recipe "mysql::client"
+
+node.set_unless[:mediawiki][:installdbPass] = node[:mysql][:server_root_password]
+
+node.set_unless[:mediawiki][:wgDBpassword] = secure_password
+node.set_unless[:mediawiki][:dbAdminPass] = secure_password
+node.set_unless[:mediawiki][:wgSecretKey] = secure_password
+node.set_unless[:mediawiki][:wgUpgradeKey] = secure_password
+
+template "/tmp/set_pass.sql" do
+ source "set_pass.sql.erb"
+ owner "root"
+ group "root"
+ mode "0640"
+ not_if do File.exists?(node[:mediawiki][:directory]) end
+end
+
+remote_file "/tmp/mediawiki-1.17.0.tar.gz" do
+ source "http://download.wikimedia.org/mediawiki/1.17/mediawiki-1.17.0.tar.gz"
+ owner "root"
+ group "root"
+ mode "0640"
+ not_if do File.exists?(node[:mediawiki][:directory]) end
+end
+
+
+script "set_mediawiki" do
+  interpreter "bash"
+  user "root"
+  cwd "/tmp"
+  not_if do File.exists?(node[:mediawiki][:directory]) end
+  code <<-EOH
+  tar -zxf mediawiki-1.17.0.tar.gz
+  mv mediawiki-1.17.0 #{node[:mediawiki][:directory]}
+  chown -R #{node[:apache][:user]}:#{node[:apache][:group]} #{node[:mediawiki][:directory]}
+  cd #{node[:mediawiki][:directory]}
+  php maintenance/install.php \
+    --dbname #{node[:mediawiki][:wgDBname]} \
+    --dbpass #{node[:mediawiki][:wgDBpassword]} \
+    --dbserver #{node[:mediawiki][:wgDBserver]} \
+    --dbuser #{node[:mediawiki][:wgDBuser]} \
+    --installdbpass #{node[:mediawiki][:installdbPass]} \
+    --pass #{node[:mediawiki][:dbAdminPass]} \
+    --installdbuser root \
+    --lang #{node[:mediawiki][:wgLanguageCode]} \
+    #{node[:mediawiki][:wgSitename]} \
+    #{node[:mediawiki][:dbAdminUser]}
+  mysql -u root -p#{node[:mediawiki][:installdbPass]} < /tmp/set_pass.sql
+  mv LocalSettings.php LocalSettings_autogenerate.php
+  EOH
+end
+
+userName=node[:apache][:user]
+groupName=node[:apache][:group]
+
+template node[:mediawiki][:directory]+"/LocalSettings.php" do
+ source "LocalSettings.php.erb"
+ owner userName
+ group groupName
+ mode "0640"
+end
+
+directory node[:mediawiki][:directory]+"/config" do
+  owner userName
+  group groupName
+  mode "0755"
+  only_if {node[:mediawiki][:access2config_folder]=="true"}
+end
+
+directory node[:mediawiki][:directory]+"/mw-config" do
+  owner userName
+  group groupName
+  mode "0755"
+  only_if {node[:mediawiki][:access2config_folder]=="true"}
+end
+
+directory node[:mediawiki][:directory]+"/config" do
+  owner userName
+  group groupName
+  mode "0400"
+  only_if {node[:mediawiki][:access2config_folder]=="false"}
+end
+
+directory node[:mediawiki][:directory]+"/mw-config" do
+  owner userName
+  group groupName
+  mode "0400"
+  only_if {node[:mediawiki][:access2config_folder]=="false"}
+end
---
+++ mediawiki/recipes/restore.rb
@@ -0,0 +1,39 @@
+#
+# Cookbook Name:: mediawiki
+# Recipe:: restore
+#
+# Copyright 2011, ccat
+#
+
+script "restore_mediawiki_by_sql" do
+  interpreter "bash"
+  user "root"
+  cwd "/tmp"
+  only_if do File.exists?(node[:mediawiki][:directory]+"/backup.sql") end
+  code <<-EOH
+    mysql -u #{node[:mediawiki][:dbuser]} -p#{node[:mediawiki][:dbpass]} #{node[:mediawiki][:dbname]} < #{node[:mediawiki][:directory]}/backup.sql
+    rm -f  #{node[:mediawiki][:directory]}/backup.sql
+  EOH
+end
+
+script "restore_mediawiki_by_tar_gz" do
+  interpreter "bash"
+  user "root"
+  cwd node[:mediawiki][:directory]
+  only_if do File.exists?(node[:mediawiki][:directory]+"/backup.tar.gz") end
+  code <<-EOH
+    cd #{node[:mediawiki][:directory]}
+    tar xvzf backup.tar.gz
+    chown -R apache:apache images
+    chown -R apache:apache skins
+    chown -R apache:apache extensions
+    php maintenance/importDump.php backup.xml
+    php maintenance/rebuildrecentchanges.php
+    rm -rf backup.xml
+    rm -rf backup.tar.gz
+  EOH
+end
+
+
+
+
